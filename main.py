174bcb607d11bde476272a0a2e22131b275939a9
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-06-21 16:40:43 -0400

    changes

--- main.py
+++ main.py
@@ -37,64 +37,70 @@ model.update()
 
 #Restricciones
 
-##R1
-model.addConstrs((s[p, e, t]* pl.GR(e) <= pl.A(p, e) for p in range(P) for e in range(E) for t in range(T)), name ="R1")
+def addAllConstrs(model):
+    ##R1
+    model.addConstrs((s[p, e, t]* pl.GR(e) <= pl.A(p, e) for p in range(P) for e in range(E) for t in range(T)), name ="R1")
 
-##R2
-model.addConstrs((s[p, e, t] == s[p, e, (t - 1)] + r[p, e, t] for p in range(P) for e in range(E) for t in range(1, T)), name = "R2")
+    ##R2
+    model.addConstrs((s[p, e, t] == s[p, e, (t - 1)] + r[p, e, t] for p in range(P) for e in range(E) for t in range(1, T)), name = "R2")
 
-##R3
-model.addConstrs((s[p, e, 0] == r[p, e, 0] for p in range(P) for e in range(E)), name = "R3")
+    ##R3
+    model.addConstrs((s[p, e, 0] == r[p, e, 0] for p in range(P) for e in range(E)), name = "R3")
 
-##R4
-model.addConstrs((op[p, e, t] <= s[p, e, t] for p in range(P) for e in range(E) for t in range(T)), name = "R4")
+    ##R4
+    model.addConstrs((op[p, e, t] <= s[p, e, t] for p in range(P) for e in range(E) for t in range(T)), name = "R4")
 
-##R5
-model.addConstrs(quicksum(et[p, q, t] for q in range(P)) <= quicksum(pl.B(p, e, t) * op[p, e, t] * pl.GR(e) for e in range(E) ) for p in range(P) for t in range(T))  # q = p' :)
+    ##R5
+    model.addConstrs(quicksum(et[p, q, t] for q in range(P)) <= quicksum(pl.B(p, e, t) * op[p, e, t] * pl.GR(e) for e in range(E) ) for p in range(P) for t in range(T))  # q = p' :)
 
-##R6
-model.addConstrs((quicksum(quicksum(s[p, e, t] * pl.GR(e) * pl.B(p, e, t) for p in range(P)) for e in range(E)) >= pl.MC(t) for t in range(T)))
+    ##R6
+    model.addConstrs((quicksum(quicksum(s[p, e, t] * pl.GR(e) * pl.B(p, e, t) for p in range(P)) for e in range(E)) >= pl.MC(t) for t in range(T)))
 
-##R7
-model.addConstrs((quicksum(pl.B(p, e, t) * op[p, e, t] * pl.GR(e) for e in range(E)) + quicksum(et[q, p, t] - et[p, q, t] for q in range(P)) >= pl.D(p, t) for p in range(P) for t in range(T)), name = "R7")
+    ##R7
+    model.addConstrs((quicksum(pl.B(p, e, t) * op[p, e, t] * pl.GR(e) for e in range(E)) + quicksum(et[q, p, t] - et[p, q, t] for q in range(P)) >= pl.D(p, t) for p in range(P) for t in range(T)), name = "R7")
 
-##R8
-model.addConstrs((s[p, e, t] >= pl.TM(e) * y[p, e, t] for p in range(P) for e in range(E) for t in range(T)), name = "R8")
+    ##R8
+    model.addConstrs((s[p, e, t] >= pl.TM(e) * y[p, e, t] for p in range(P) for e in range(E) for t in range(T)), name = "R8")
 
-##R9
-model.addConstrs((s[p, e, t] <= y[p, e, t] * BigM for p in range(P) for e in range(E) for t in range(T)), name = "R9a")
-model.addConstrs((y[p, e, t] <= s[p, e, t] for p in range(P) for e in range(E) for t in range(T)), name = "R9b")
+    ##R9
+    model.addConstrs((s[p, e, t] <= y[p, e, t] * BigM for p in range(P) for e in range(E) for t in range(T)), name = "R9a")
+    model.addConstrs((y[p, e, t] <= s[p, e, t] for p in range(P) for e in range(E) for t in range(T)), name = "R9b")
 
-##R10
-model.addConstrs((quicksum(r[p, e, t] for e in range(E) for p in range(P)) <= (6_500 * P) for t in range(T)), name = "R10")
+    ##R10
+    model.addConstrs((quicksum(r[p, e, t] for e in range(E) for p in range(P)) <= (6_500 * P) for t in range(T)), name = "R10")
 
-##Naturaleza de las Variables
-model.addConstrs((et[p, q, t] >= 0 for p in range(P) for q in range(P) for t in range(T)), name="R10")
+    ##Naturaleza de las Variables
+    model.addConstrs((et[p, q, t] >= 0 for p in range(P) for q in range(P) for t in range(T)), name="R10")
 
 
-#Funcion Objetivo
+    #Funcion Objetivo
 
-objetivo1 = quicksum(quicksum(quicksum(op[p, e, t] * pl.GR(e) * pl.B(p, e, t) * pl.PE(t) for t in range(T)) for e in range(E)) for p in range(P))
-objetivo2 = quicksum(quicksum(quicksum(pl.CM(e , t) * s[p, e, t] + pl.CI(p, e, t) * r[p, e, t] for t in range(T)) for e in range(E)) for p in range(P))
-objetivo3 = quicksum(quicksum(quicksum(et[p, q, t] * pl.CT(t) * pl.DT(p, q) for t in range(T)) for q in range(P) if p != q) for p in range(P)) 
-objetivo4 = quicksum(quicksum(quicksum(pl.CO(e, t) * op[p, e, t] for t in range(T)) for e in range(E) if e != 3) for p in range(P))
+    objetivo1 = quicksum(quicksum(quicksum(op[p, e, t] * pl.GR(e) * pl.B(p, e, t) * pl.PE(t) for t in range(T)) for e in range(E)) for p in range(P))
+    objetivo2 = quicksum(quicksum(quicksum(pl.CM(e , t) * s[p, e, t] + pl.CI(p, e, t) * r[p, e, t] for t in range(T)) for e in range(E)) for p in range(P))
+    objetivo3 = quicksum(quicksum(quicksum(et[p, q, t] * pl.CT(t) * pl.DT(p, q) for t in range(T)) for q in range(P) if p != q) for p in range(P)) 
+    objetivo4 = quicksum(quicksum(quicksum(pl.CO(e, t) * op[p, e, t] for t in range(T)) for e in range(E) if e != 3) for p in range(P))
 
-objetivo = objetivo1 - objetivo2 - objetivo3 - objetivo4
+    objetivo = objetivo1 - objetivo2 - objetivo3 - objetivo4
+    model.setObjective(objetivo, GRB.MAXIMIZE)
 
-model.setObjective(objetivo, GRB.MAXIMIZE)
-model.optimize()
+def optimizeAndPrintResults():
+    addAllConstrs(model)
+    model.optimize()
 
-#Guarda los valores de las variables de decision
-s_values_array = np.zeros((P, E, T))
-op_values_array = np.zeros((P, E, T))
-for p in range(P):
-    for e in range(E):
-        for t in range(T):
-            s_values_array[p, e, t] = s[p, e, t].X
-            op_values_array[p, e, t] = op[p, e, t].X
+    #Guarda los valores de las variables de decision
+    s_values_array = np.zeros((P, E, T))
+    op_values_array = np.zeros((P, E, T))
+    for p in range(P):
+        for e in range(E):
+            for t in range(T):
+                s_values_array[p, e, t] = s[p, e, t].X
+                op_values_array[p, e, t] = op[p, e, t].X
 
-np.save(os.path.join("outputs", "op_values"), op_values_array)
+    np.save(os.path.join("outputs", "op_values"), op_values_array)
 
 #model.computeIIS()
 #archivo = "encontrar_infactibilidad"
 #model.write(f"{archivo}.ilp")
+
+if __name__ == "__main__":
+    optimizeAndPrintResults()
---
+++ sensitivityAnalisis.py
@@ -0,0 +1,75 @@
+import sys
+import gurobipy as gp
+import main
+import paramLoading as pl
+import matplotlib.pyplot as plt
+from gurobipy import GRB, Model, quicksum
+
+
+def generateGraphics(k_values, z_values, modified_value_name):
+    # Graficar
+    plt.plot(k_values, z_values, color = 'b', linestyle = '-', 
+            marker = 'o',label = "z vs k") 
+
+    plt.xticks(rotation = 25) 
+    plt.xlabel('Valor de multiplicador') 
+    plt.ylabel('Valor óptimo') 
+    plt.title(f'Valor óptimo en función de multiplicador que altera {modified_value_name}', fontsize = 20)
+    plt.grid() 
+    plt.legend()
+    # Guardar gráfico como png
+    plt.savefig(f'outputs/sensitivity_analysis_{modified_value_name}.png')
+
+
+def sensitivityCIpet(k):
+    # Nuevo objetivo ajustando CI
+    # Funcion Objetivo
+
+    print(f"Current value of multiplier: {k}")
+    main.objetivo2 = quicksum(quicksum(quicksum(pl.CM(e , t) * main.s[p, e, t] + pl.CI(p, e, t) * k
+                                                * main.r[p, e, t] for t in range(main.T))
+                                                for e in range(main.E)) for p in range(main.P))
+    main.objetivo = main.objetivo1 - main.objetivo2 - main.objetivo3 - main.objetivo4
+
+    main.model.setObjective(main.objetivo, GRB.MAXIMIZE)
+    main.optimizeAndPrintResults()
+
+    # Cada vez que modelo optimiza, guardar en lista valor óptimo y multiplicador:
+    return main.model.objVal
+
+
+def sensitivityB(k, B):
+    main.pl.B = lambda p, e, t: B(p,e,t) * (1 + k)
+    main.optimizeAndPrintResults()
+
+    return main.model.objVal
+
+
+if __name__ == "__main__":
+
+    parameter = sys.argv[1]
+
+    if parameter == "CI":
+        # Alterar valores de CI
+        k_values = []
+        z_values = []
+        K = [1e1, 1e2, 1e3, 1e4]
+        for k in K:
+            obj_val = sensitivityCIpet(k)
+            k_values.append(k), z_values.append(obj_val)
+
+        generateGraphics(k_values, z_values, "CI")
+
+    if parameter == "B":
+        # Alterar valores de B
+        k_values = []
+        z_values = []
+        K = [-0.5, -0.25, 0.25, 0.5]
+        B = pl.B
+        for k in K:
+            obj_val = sensitivityB(k, B)
+            k_values.append(k), z_values.append(obj_val)
+
+        print(k_values, z_values)
+
+        generateGraphics(k_values, z_values, "B")
